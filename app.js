194b338d564a9cbbfc00be7b3d66d2814fe2aae5
Safwan <35345@ma-web.nl>
2022-11-22 12:32:15 +0100

    here

---
+++ app.js
@@ -0,0 +1,32 @@
+class App 
+{
+    runApplication ()
+    {
+        
+        console.log("Hello")
+        let appName = ("Avalon")
+        let Version =(0.9)
+        let DateVer =(12/8/2003)
+        let author =("Safwan")
+        let copyright =("your_mom")
+        let distrubuter =("your_dad")
+        let darkmode = ("bool")
+        console.log(appName)
+        console.log(Version)
+        console.log(DateVer)
+        console.log(author)
+        console.log(copyright)
+        console.log(distrubuter)
+        console.log(darkmode)
+
+
+
+
+
+
+        
+    }
+}
+
+let app = new App ();
+app.runApplication();
