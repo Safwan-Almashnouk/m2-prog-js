e45961a839c81af32a9d971b18513c40fda25962
Safwan <35345@ma-web.nl>
2022-11-29 15:54:22 +0100

    drawing

---
+++ canvas/app.js
@@ -0,0 +1,178 @@
+class App 
+{
+    runApplication ()
+    {
+        console.log("hello world");
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        let randomnumber = Math.random()
+        console.log(randomnumber)
+        if (randomnumber == 0.0)
+        {
+        g.beginPath()
+        g.fillStyle = "cyan";
+        g.moveTo(300,100);
+        g.lineTo(700,200);
+        g.lineTo(600,400);
+        g.lineTo(200,300);        
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "Blue";
+        g.moveTo(700,200)
+        g.lineTo(800,300);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "Purple"
+        g.moveTo(600,400)
+        g.lineTo(600,600)
+        g.lineTo(800, 500)
+        g.lineTo (800, 300)
+        g.lineTo(600, 400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "Blue"
+        g.moveTo(600, 400)
+        g.lineTo(200, 300)
+        g.lineTo(200, 500)
+        g.lineTo(600, 600)
+        g.lineTo(600,400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        }
+        else if (randomnumber < 0.2)
+        {
+        g.beginPath()
+        g.fillStyle = "pink";
+        g.moveTo(300,100);
+        g.lineTo(700,200);
+        g.lineTo(600,400);
+        g.lineTo(200,300);        
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "red";
+        g.moveTo(700,200)
+        g.lineTo(800,300);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "black"
+        g.moveTo(600,400)
+        g.lineTo(600,600)
+        g.lineTo(800, 500)
+        g.lineTo (800, 300)
+        g.lineTo(600, 400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "green"
+        g.moveTo(600, 400)
+        g.lineTo(200, 300)
+        g.lineTo(200, 500)
+        g.lineTo(600, 600)
+        g.lineTo(600,400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        }
+        else if (randomnumber > 0.6)
+        {
+        g.beginPath()
+        g.fillStyle = "green";
+        g.moveTo(300,100);
+        g.lineTo(700,200);
+        g.lineTo(600,400);
+        g.lineTo(200,300);        
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "pink";
+        g.moveTo(700,200)
+        g.lineTo(800,300);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "Purple"
+        g.moveTo(600,400)
+        g.lineTo(600,600)
+        g.lineTo(800, 500)
+        g.lineTo (800, 300)
+        g.lineTo(600, 400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "red"
+        g.moveTo(600, 400)
+        g.lineTo(200, 300)
+        g.lineTo(200, 500)
+        g.lineTo(600, 600)
+        g.lineTo(600,400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        }
+        else if (randomnumber > 0.2 || randomnumber < 0.6 )
+        {
+        g.beginPath()
+        g.fillStyle = "black";
+        g.moveTo(300,100);
+        g.lineTo(700,200);
+        g.lineTo(600,400);
+        g.lineTo(200,300);        
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "red";
+        g.moveTo(700,200)
+        g.lineTo(800,300);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "black"
+        g.moveTo(600,400)
+        g.lineTo(600,600)
+        g.lineTo(800, 500)
+        g.lineTo (800, 300)
+        g.lineTo(600, 400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath()
+        g.fillStyle = "red"
+        g.moveTo(600, 400)
+        g.lineTo(200, 300)
+        g.lineTo(200, 500)
+        g.lineTo(600, 600)
+        g.lineTo(600,400)
+        g.closePath();
+        g.stroke();
+        g.fill();
+        }
+        
+        document.getElementById("canvasid");
+        console.log(canvas); 
+        
+    }
+}
+
+let app = new App ();
+app.runApplication();
